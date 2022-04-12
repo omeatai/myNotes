6b61a47e7eff0328b967d63bafe2006a2a10c419
IFEANYI OMEATA <omeatai@gmail.com>
2022-04-12 19:51:11 +0100

    removed
    removed

--- works/income/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Income
-
-
-class IncomeSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Income
-        fields = ['id', 'date', 'description', 'amount', 'source']
