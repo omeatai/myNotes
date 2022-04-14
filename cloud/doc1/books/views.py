0c23669bb5460931a27f746739cc22e400751041
IFEANYI OMEATA <omeatai@gmail.com>
2022-04-14 17:17:24 +0100

    changes
    changes

--- cloud/doc1/books/views.py
+++ cloud/doc1/books/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from books.models import Book
 
-# Create your views here.
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'book_list.html'
