b84fd2be9144ece399ea9c0a4741b1106f47acb3
IFEANYI OMEATA <omeatai@gmail.com>
2022-04-16 11:09:56 +0000

    minor

--- cloud/doc2-todo-api/backend/todos/views.py
+++ cloud/doc2-todo-api/backend/todos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from rest_framework.generics import (ListAPIView, RetrieveAPIView)
 from .serializers import TodoSerializer
 from .models import Todo
