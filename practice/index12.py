f7718a71ee439c32d8c8dc999047e0c180b09f25
IFEANYI OMEATA <omeatai@gmail.com>
2022-05-04 17:35:00 +0100

    changes
    changes

--- practice/index12.py
+++ practice/index12.py
@@ -1,7 +1,7 @@
-def is_palindrome(value):
-    val = value.replace(" ","").lower()
-    if val == val[::-1]:
-        return True
-    return False
+# def is_palindrome(value):
+#     val = value.replace(" ","").lower()
+#     if val == val[::-1]:
+#         return True
+#     return False
 
-print(is_palindrome('ana a  aana'))
+# print(is_palindrome('ana a  aana'))
